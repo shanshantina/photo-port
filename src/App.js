0d4f3442a8b9c0a095a1477524dfcb7171b83379
Shanshan Xu <tinaxu84@gmail.com>
2021-04-10 14:22:46 -0400

    about nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // JSX(JavaScript XML) that can represent HTML in JavaScript
+    /* React.createElement('div", {}, [
+       React.createElement('h1', {}, 'Ginger'),
+       React.createElement('p', {}, 'breed: Brittany Spaniel'),
+       ]) */
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
