aa965a5b2783e23bab69d01c78693133f3739d9e
Shanshan Xu <tinaxu84@gmail.com>
2021-04-10 17:58:54 -0400

    add gallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import About from './components/About';
-import Nav from './components/Nav';
+import React, { useState } from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     // JSX(JavaScript XML) that can represent HTML in JavaScript
     /* React.createElement('div", {}, [
@@ -10,8 +27,13 @@ function App() {
        React.createElement('p', {}, 'breed: Brittany Spaniel'),
        ]) */
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+  // in here we use array and .map method to return the list navigators
+  // When we map over an array in a JSX expression, we should return only a single JSX element
 
-function Nav() {
-    const categories = [
-        {
-          name: "commercial",
-          description:"Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
-
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
-    // in here we use array and .map method to return the list navigators
-    // When we map over an array in a JSX expression, we should return only a single JSX element
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
@@ -31,14 +16,23 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => categorySelected()}>About Me</a>
+            <a data-testid="about" href="#about">
+              About Me
+            </a>
           </li>
-          <li >
-            <span onClick={() => categorySelected()}>Contact</span>
+          <li className="mx-2">
+            <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>{capitalizeFirstLetter(category.name)}</span>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name
+              }`}
+              key={category.name}
+            >
+              <span onClick={() => {setCurrentCategory(category)}}>
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
           ))}
         </ul>
